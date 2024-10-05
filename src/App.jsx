fb02ffe5501142a9df4cf8342613bab6ad3585ff
StackCanary <danizmd8@gmail.com>
2024-10-05 19:56:59 +0300

    feat: compose last two added compontents together

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,50 @@ import { useState } from "react";
 import SessionSettingPage from "./pages/SessionSettingPage";
 import SecondsSettingPage from "./pages/SecondsSettingPage";
 import TimerPage from "./pages/TimerPage";
+import DeepBreathPage from "./pages/DeepBreathPage";
+import EndSessinoPage from "./pages/EndSessionPage";
 
 const App = () => {
   const [roundsAmount, setRoundsAmount] = useState(null);
   const [lastSecondsToHoldBreath, setLastSecondsToHoldBreath] = useState(0);
   const [secondsToHoldBreath, setSecondsToHoldBreath] = useState(0);
+  const [isInterbreathing, setIsInterbreathing] = useState(false);
+  const [isAfterholding, setIsAfterholding] = useState(false);
+  const [isBreathingBeforeAfterholding, setIsBreathingBeforeAfterholding] =
+    useState(false);
+
+  const afterDeepBreath = () => {
+    setIsBreathingBeforeAfterholding(false);
+    setIsAfterholding(true);
+  };
 
   const finishTimer = () => {
+    setIsBreathingBeforeAfterholding(true);
+  };
+
+  const finishAfterholding = () => {
+    setIsAfterholding(false);
     setLastSecondsToHoldBreath(secondsToHoldBreath);
     setSecondsToHoldBreath(0);
+    setRoundsAmount(roundsAmount - 1);
   };
+
+  const onSecondsSelected = (seconds) => {
+    setIsInterbreathing(true);
+    setSecondsToHoldBreath(seconds);
+  };
+
+  if (roundsAmount !== null && roundsAmount <= 0) {
+    return <EndSessinoPage />;
+  }
+
+  if (isBreathingBeforeAfterholding) {
+    return <DeepBreathPage onFinish={afterDeepBreath} />;
+  }
+
+  if (isAfterholding) {
+    return <TimerPage duration={3} onFinish={finishAfterholding} />;
+  }
 
   if (roundsAmount === null) {
     return <SessionSettingPage onRoundsSelected={setRoundsAmount} />;
@@ -21,16 +55,12 @@ const App = () => {
     return (
       <SecondsSettingPage
         initialValue={lastSecondsToHoldBreath}
-        onSecondsSelected={setSecondsToHoldBreath}
+        onSecondsSelected={onSecondsSelected}
       />
     );
   }
 
-  return (
-    <>
-      <TimerPage duration={secondsToHoldBreath} onFinish={finishTimer} />
-    </>
-  );
+  return <TimerPage duration={secondsToHoldBreath} onFinish={finishTimer} />;
 };
 
 export default App;
